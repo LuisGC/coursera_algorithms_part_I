ea05826378e8f38ce0e8794defae79d14636e991
LuisGC <luisgc@gmail.com>
2014-09-27 22:25:38 +0200

    first implementation (and test) of Deque

---
+++ DequesAndRandomizedQueuesAssignment/courseraAlgorithmsPartIWeek2/src/Deque.java
@@ -0,0 +1,91 @@
+import java.util.Iterator;
+
+/**
+ * @author luisgc
+ */
+public class Deque<Item> implements Iterable<Item> {
+
+    /**
+     * construct an empty deque.
+     */
+    public Deque() {
+        // TODO
+    }
+
+    /**
+     * is the deque empty?
+     * 
+     * @return boolean
+     */
+    public boolean isEmpty() {
+        // TODO
+        return false;
+    }
+
+    /**
+     * return the number of items on the deque.
+     * 
+     * @return size
+     */
+    public int size() {
+        // TODO
+        return 0;
+    }
+
+    /**
+     * insert the item at the front.
+     * 
+     * @param item
+     *            item to add
+     */
+    public void addFirst(Item item) {
+        // TODO
+    }
+
+    /**
+     * insert the item at the end.
+     * 
+     * @param item
+     *            item to add
+     */
+    public void addLast(Item item) {
+        // TODO
+    }
+
+    /**
+     * delete and return the item at the front.
+     * 
+     * @return Item
+     */
+    public Item removeFirst() {
+        // TODO
+        return null;
+    }
+
+    /**
+     * delete and return the item at the end.
+     * 
+     * @return Item
+     */
+    public Item removeLast() {
+        // TODO
+        return null;
+    }
+
+    /**
+     * return an iterator over items in order from front to end.
+     */
+    @Override
+    public Iterator<Item> iterator() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+}
---
+++ DequesAndRandomizedQueuesAssignment/courseraAlgorithmsPartIWeek2/test/DequeTest.java
@@ -0,0 +1,120 @@
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.util.NoSuchElementException;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author luisgc
+ */
+public class DequeTest {
+
+    private static final Integer ITEM_ONE = Integer.valueOf(1);
+    private static final Integer ITEM_TWO = Integer.valueOf(2);
+
+    Deque<Integer> deque;
+
+    /**
+     * @throws java.lang.Exception
+     */
+    @Before
+    public void setUp() throws Exception {
+        deque = new Deque<Integer>();
+    }
+
+    /**
+     * Test method for {@link Deque#Deque()}.
+     */
+    @Test
+    public final void testConstructor() {
+        assertNotNull(deque);
+        assertEquals(0, deque.size());
+    }
+
+    /**
+     * Test method for {@link Deque#isEmpty()}.
+     */
+    @Test
+    public final void testIsEmpty() {
+        assertTrue(deque.isEmpty());
+        deque.addFirst(ITEM_ONE);
+        assertFalse(deque.isEmpty());
+    }
+
+    /**
+     * Test method for {@link Deque#size()}.
+     */
+    @Test
+    public final void testSize() {
+        assertEquals(0, deque.size());
+
+        deque.addFirst(ITEM_ONE);
+        assertEquals(1, deque.size());
+        deque.removeFirst();
+        assertEquals(0, deque.size());
+
+        deque.addLast(ITEM_TWO);
+        assertEquals(1, deque.size());
+        deque.removeLast();
+        assertEquals(0, deque.size());
+    }
+
+    /**
+     * Test method for {@link Deque#addFirst(java.lang.Object)}.
+     */
+    @Test
+    public final void testAddFirst() {
+        assertEquals(0, deque.size());
+
+        deque.addFirst(ITEM_ONE);
+        assertEquals(1, deque.size());
+        assertEquals(ITEM_ONE, deque.removeFirst());
+
+        deque.addFirst(ITEM_TWO);
+        assertEquals(1, deque.size());
+        assertEquals(ITEM_TWO, deque.removeLast());
+
+    }
+
+    /**
+     * Test method for {@link Deque#addLast(java.lang.Object)}.
+     */
+    @Test
+    public final void testAddLast() {
+        assertEquals(0, deque.size());
+
+        deque.addLast(ITEM_ONE);
+        assertEquals(1, deque.size());
+        assertEquals(ITEM_ONE, deque.removeFirst());
+
+        deque.addLast(ITEM_TWO);
+        assertEquals(1, deque.size());
+        assertEquals(ITEM_TWO, deque.removeLast());
+
+    }
+
+    /**
+     * Test method for {@link Deque#removeFirst()}.
+     */
+    @Test(expected = NoSuchElementException.class)
+    public final void testRemoveFirstEmpty() {
+        assertEquals(0, deque.size());
+
+        deque.removeFirst();
+    }
+
+    /**
+     * Test method for {@link Deque#removeLast()}.
+     */
+    @Test(expected = NoSuchElementException.class)
+    public final void testRemoveLastEmpty() {
+        assertEquals(0, deque.size());
+
+        deque.removeLast();
+    }
+
+}
